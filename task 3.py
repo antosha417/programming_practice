e5abe18c8277b4da3c49b68518580dad4870a531
Lisa Dubodel <lisiaklisiji@gmail.com>
2019-09-21 15:57:56 +0300

    task 3 and 4

---
+++ task 3.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+c = int(input())
+lst = [a, b, c]
+print(min(lst))
---
+++ task4.py
@@ -0,0 +1,4 @@
+v = int(input())
+t = int(input())
+if v>=0: print(abs(109-v*t))
+else: print(109+(v*t+109))
