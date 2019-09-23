b0a4e7416f0d632ecf079595f9efb9ac0ea8c058
MalikovArtur <malikov.a@phystech.edu>
2019-09-23 21:44:26 +0300

    7, 8, 9 задачи

---
+++ ex7.py
@@ -0,0 +1,4 @@
+a = [1, 'treg', 67, 8, 'huter']
+for i in range(len(a)):
+    if i % 2 == 0:
+        print(a[i])
---
+++ ex8.py
@@ -0,0 +1,4 @@
+a = [1, 6, 67, 8, 24, 0]
+for i in range(len(a)):
+    if a[i] % 2 == 0:
+        print(a[i])
