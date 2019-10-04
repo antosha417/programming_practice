5b5fb65c7cddefb1b7776778215a5338f64aca3d
LA-P <turubanova666@yandex.ru>
2019-10-05 01:30:23 +0300

    Черепашки с 1 по 8 задания

---
+++ Спираль-квадрат.py
@@ -0,0 +1,7 @@
+import turtle
+
+turtle.shape('turtle')
+
+for i in range (1,1000,5):
+    turtle.left(90)
+    turtle.forward(i+10)
---
+++ Черепашка окружность.py
@@ -0,0 +1,6 @@
+import turtle
+turtle.shape('turtle')
+
+for i in range (400):
+    turtle.forward(1)
+    turtle.left(1)
