26860db47cea74e2c77a6cf620759c86ae78fd86
polinapgv <polina.pgv@yandex.ru>
2019-10-05 18:52:27 +0300

    week 3 task 3

---
+++ w3t3.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
