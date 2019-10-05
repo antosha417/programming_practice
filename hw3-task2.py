86f7f1455aa47265a52ae9f14c0f92227f01a188
Alisa <aliceinmirror@inbox.ru>
2019-10-06 00:15:37 +0300

    Аналогично первому заданию. Черепаха работает

---
+++ hw3-task2.py
@@ -0,0 +1,14 @@
+#Сохраните и выполните предыдущую программу. Убедитесь в том, что черепаха работает.
+if __name__ == '__main__':
+    import turtle
+
+    turtle.shape('turtle')
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
