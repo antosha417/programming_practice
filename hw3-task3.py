4b8a45bba1a05f39ec1ddfb028b4c57ad01b4304
Alisa <aliceinmirror@inbox.ru>
2019-10-06 00:23:10 +0300

    Решенная задача номер три.

---
+++ hw3-task3.py
@@ -0,0 +1,11 @@
+#Нарисуйте квадрат.
+if __name__ == '__main__':
+    import turtle
+    turtle.shape("turtle")
+    turtle.forward(90)
+    turtle.left(90)
+    turtle.forward(90)
+    turtle.left(90)
+    turtle.forward(90)
+    turtle.left(90)
+    turtle.forward(90)
