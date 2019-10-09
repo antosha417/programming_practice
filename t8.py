19087dfff78433102ca899c40c7bb23745b91c3a
polinapgv <polina.pgv@yandex.ru>
2019-10-09 09:52:36 +0300

    week2 task8 edited

--- t8.py
+++ t8.py
@@ -3,6 +3,6 @@ c = int(input())
 while c != 0:
     a.append(c)
     c = int(input())
-for i in range(len(a)):
-    if a[i] % 2 == 0:
-        print(a[i])
+for elem in a:
+    if elem % 2 == 0:
+        print(elem)
