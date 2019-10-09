2af4adc66b24fd811b9512a0e18a5e04c93aa9e4
PavelTin <pavel_tin@mail.ru>
2019-10-10 02:44:39 +0300

    Tu11

--- week03/Turtle11.py
+++ week03/Turtle11.py
@@ -3,14 +3,14 @@ from math import sin,pi
 
 def draw_2circles(R):
     n=36
-    angle=360/n
-    a=2*R*sin(angle/2*pi/180)
+    angle=180-360/n
+    a=2*R*sin(360/n/2*pi/180)
     for i in range(n):
         turtle.forward(a)
-        turtle.left(angle)
+        turtle.left(180-angle)
     for i in range(n):
         turtle.forward(a)
-        turtle.right(angle)
+        turtle.right(180-angle)
 N=4
 r=20
 dr=10
