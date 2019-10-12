95d441fca172f62de388e49796d2abb92b370fd1
polinapgv <polina.pgv@yandex.ru>
2019-10-12 23:31:11 +0300

    week4 task10

--- week4/w4t10.py
+++ week4/w4t10.py
@@ -1,2 +1,11 @@
 n = int(input())
+summary = {}
 for i in range(n):
+    s = list(map(str, input().split()))
+    if summary.get(s[0]) is None:
+        summary[s[0]] = {}
+    if summary[s[0]].get(s[1]) is not None:
+        summary[s[0]][s[1]] += int(s[2])
+    else:
+        summary[s[0]][s[1]] = int(s[2])
+print(summary)
