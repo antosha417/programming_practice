9d3fd040a8ad7b802c4bbf13e222f7d7f43debb7
PavelTin <pavel_tin@mail.ru>
2019-10-19 15:48:03 +0300

    py5 замена sum на summar

--- week02/Py5.py
+++ week02/Py5.py
@@ -1,10 +1,10 @@
 def sum_factorials(n):
     a=1
-    sum=0
+    summar=0
     for i in range(1,n+1):
         a=a*i
-        sum+=a
-    print(sum)
+        summar+=a
+    print(summar)
 
 n=int(input())
 sum_factorials(n)
