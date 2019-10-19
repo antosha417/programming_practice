79039cee44cdcb396f1d65a86814a4605892b7ce
polinapgv <polina.pgv@yandex.ru>
2019-10-19 14:35:57 +0300

    week3 task11 edited

--- week3/w3t11.py
+++ week3/w3t11.py
@@ -1,19 +1,19 @@
 import turtle
 
-def circleleft(n):
-    for i in range(180):
-        turtle.forward(n)
-        turtle.left(2)
-
-def circleright(n):
-    for i in range(180):
-        turtle.forward(n)
-        turtle.right(2)
+def circle(n, *, direction):
+    if direction == 'left':
+        for i in range(180):
+            turtle.forward(n)
+            turtle.left(2)
+    else:
+        for i in range(180):
+            turtle.forward(n)
+            turtle.right(2)
 
 turtle.shape('turtle')
 turtle.left(90)
 l = 0
 for i in range(1, 11):
     l += 0.5
-    circleleft(l)
-    circleright(l)
+    circle(l, direction = 'left')
+    circle(l, direction = 'right')
