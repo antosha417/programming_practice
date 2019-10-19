4c94e2c6ca6a569c131a158fea613dbb55ea095b
polinapgv <polina.pgv@yandex.ru>
2019-10-19 16:53:53 +0300

    week4 task8 edited

--- week4/w4t8.py
+++ week4/w4t8.py
@@ -1,12 +1,8 @@
 n = int(input())
 dict_syn = {}
 for i in range(n):
-    s = list(map(str, input().split()))
-    dict_syn[s[0]] = s[1]
-word = str(input())
-if dict_syn.get(word) is not None:
-    print(dict_syn[word])
-else:
-    for key, value in dict_syn.items():
-        if value == word:
-            print(key)
+    word, synonym = input().split()
+    dict_syn[word] = synonym
+    dict_syn[synonym] = word
+word_orig = str(input())
+print(dict_syn[word_orig])
