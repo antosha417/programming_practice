1598a0660fe0c0f317f59b0dcbaf732ce2bfa9aa
polinapgv <polina.pgv@yandex.ru>
2019-10-19 14:41:11 +0300

    week4 task1 edited

--- week4/w4t1.py
+++ week4/w4t1.py
@@ -1,9 +1,9 @@
-a = []
-k = 0
-n = int(input())
-for i in range(n):
-    a.append(int(input()))
-for i in range(1, n - 1):
-    if a[i] >  a[i - 1] and a[i] > a[i + 1]:
-        k += 1
-print(k)
+arr = []
+amount = 0
+num = int(input())
+for i in range(num):
+    arr.append(int(input()))
+for i in range(1, num - 1):
+    if arr[i] >  arr[i - 1] and arr[i] > arr[i + 1]:
+        amount += 1
+print(amount)
