a1fa0bdca5fddc928fe924d76385f9c9db996f6a
polinapgv <polina.pgv@yandex.ru>
2019-10-19 14:38:33 +0300

    week4 task1 edited

--- week4/w4t1.py
+++ week4/w4t1.py
@@ -2,8 +2,7 @@ a = []
 k = 0
 n = int(input())
 for i in range(n):
-    c = int(input())
-    a.append(c)
+    a.append(int(input()))
 for i in range(1, n - 1):
     if a[i] >  a[i - 1] and a[i] > a[i + 1]:
         k += 1
