93a0d7d5eab575c1fc7fdeb6607d9ab4c63e2394
polinapgv <polina.pgv@yandex.ru>
2019-10-21 17:31:11 +0300

    week4 task7 edited

--- week4/w4t7.py
+++ week4/w4t7.py
@@ -1,17 +1,7 @@
 n = int(input())
-n_general = 0
-s_general = []
-f, k = 0, 0
+text = set()
 for i in range(n):
-    s_current = list(map(str, input().split()))
-    n_current = len(s_current)
-    s_general.append(s_current)
-    for j in range(n_current):
-        for k in range(n_general + j):
-            if s_current[j] == s_general[k]:
-                f = 1
-        if f == 0:
-            k += 1
-        f = 0
-    n_general += n_current
-print(k)
+    s = input().split()
+    for elem in s:
+        text.add(elem)
+print(len(text))
