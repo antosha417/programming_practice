299d817f04a253a845f053e4558a48f5a7aae4a7
PavelTin <pavel_tin@mail.ru>
2019-11-02 18:17:38 +0300

    4py3 упростил программу, убрал приравнивание к пустой строке, начал второй цикл со счетчика старого

--- week04/4py3.py
+++ week04/4py3.py
@@ -1,16 +1,11 @@
 def num_pairs(a):
     count = 0
-    number = 1
     for i in range(len(a)):
-        if a[i] != '':
-            for j in range(len(a)):
-                if a[i] == a[j] and i != j:
-                    number += 1
-                    a[j] = ''
-            a[i] = ''
-            count += number * (number - 1) / 2
-            number = 1
+        for j in range(i+1, len(a)):
+            if a[i] == a[j]:
+                count += 1
     print(count)
 
-arr=[int(s) for s in input().split()]
+
+arr = [int(s) for s in input().split()]
 num_pairs(arr)
