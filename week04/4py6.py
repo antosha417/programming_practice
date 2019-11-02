8af7cfd928c052ea8f1413abaad41ae938094ef8
PavelTin <pavel_tin@mail.ru>
2019-11-02 18:19:57 +0300

    4py6 реформатировал

--- week04/4py6.py
+++ week04/4py6.py
@@ -1,11 +1,11 @@
 def was_earlier(row):
     a = set()
     for i in range(len(row)):
-        if int(row[i] in a) == 0:
+        if row[i] in a:
+            print('YES')
+        else:
             a.add(row[i])
             print('NO')
-        else:
-            print('YES')
 
 row = [int(elem) for elem in input().split()]
 was_earlier(row)
