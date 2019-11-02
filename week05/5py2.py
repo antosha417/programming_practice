4dec18dd4a7fe26cf555b383bc92ec9f5298cc28
PavelTin <pavel_tin@mail.ru>
2019-11-02 21:05:00 +0300

    5py2 проверка на int с помощью isinstance(), в данном случае не совсем подходит, но пусть будет

--- week05/5py2.py
+++ week05/5py2.py
@@ -1,23 +1,29 @@
 def max_common_div(arr):
-    A=set()
+    A = set()
     for i in range(N):
-        a=arr[i]
-        for j in range(i+1,N):
-            b=arr[j]
-            minelem=min(a,b)
-            for k in range(minelem,-1,-1):
-                if ((a%k==0) and (b%k==0)):
+        a = arr[i]
+        for j in range(i + 1, N):
+            b = arr[j]
+            minelem = min(a, b)
+            for k in range(minelem, -1, -1):
+                if ((a % k == 0) and (b % k == 0)):
                     A.add(k)
                     break
 
     return A
-mas=[]
-N=int(input())
-for i in range(N):
-    a=int(input())
-    mas.append(a)
-B=max_common_div(mas)
+
+
+mas = []
+N = int(input())
+if isinstance(N,int):
+    for i in range(N):
+        a = int(input())
+        if isinstance(a,int):
+            mas.append(a)
+        else:
+            print('Type int value')
+else:
+    print('Type int number')
+B = max_common_div(mas)
 for elem in B:
     print(elem, ' ')
-
-
