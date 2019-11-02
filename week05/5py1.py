882b2f83eebab6168b52e026930bbcb3d7724818
PavelTin <pavel_tin@mail.ru>
2019-11-02 18:46:12 +0300

    5py1 проверка на int с помощью isdigit()

--- week05/5py1.py
+++ week05/5py1.py
@@ -1,9 +1,15 @@
-def degree(a,n):
-    res=1
-    for i in range(n):
-        res*=a
-    return res
+def degree(a:any, n:any):
+    if a.isdigit() and n.isdigit():
+        a=int(a)
+        n=int(n)
+        res = 1
+        for i in range(n):
+            res *= a
+        return res
+    else:
+        print('Type int variables')
 
-a=int(input())
-n=int(input())
-print(degree(a,n))
+
+a = input()
+n = input()
+print(degree(a, n))
