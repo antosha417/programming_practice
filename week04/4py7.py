31ccc6faad0bcc9b9edb7fcd57ca7621f05397ed
PavelTin <pavel_tin@mail.ru>
2019-11-02 18:24:11 +0300

    4py7 добавил считывание всех строк + реформатировал

--- week04/4py7.py
+++ week04/4py7.py
@@ -1,11 +1,12 @@
-def num_dif_words(N,row):
+def num_dif_words(N):
     a = set()
     count = 0
     for i in range(N):
-        for elem in row:
+        lst = input().split()
+        for elem in lst:
             a.add(elem)
     print(len(a))
 
+
 num = int(input())
-lst = set(input().split())
-num_dif_words(num,lst)
+num_dif_words(num)
