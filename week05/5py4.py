46129ebc3a4451c414e52a9dddea7ba16e313ec9
PavelTin <pavel_tin@mail.ru>
2019-11-02 23:40:59 +0300

    5py4 проверка isinstance() и на int с помощью isdigit(),

--- week05/5py4.py
+++ week05/5py4.py
@@ -2,25 +2,31 @@ def count_sum(matrix,flag):
     s=0
     for i in range(N):
         for j in range(N):
-            if flag:
-                if i==j:
-                    s+=matrix[i][j]
-            elif i+j==N-1:
+            if flag>0 and i==j:
+                s+=matrix[i][j]
+            elif flag<0 and i+j==N-1:
                 s += matrix[i][j]
     return s
 
-N=int(input())
+N=input()
 arr=[]
-for i in range(N):
-    line=input().split()
-    for j in range(N):
-        line[j]=int(line[j])
-    arr.append(line)
-print('Main or side diagonal?')
-s=input()
-k=True
-if s=='main':
-    k=True
-elif s=='side':
-    k=False
-print(count_sum(arr,k))
+if not N.isdigit():
+    print('Type int value')
+else:
+    N=int(N)
+    for i in range(N):
+        line=input().split()
+        for j in range(N):
+            line[j]=int(line[j])
+        arr.append(line)
+    print('Main or side diagonal?')
+    s=input()
+    k=0
+    if isinstance(s,str):
+        if s=='main':
+            k=1
+        elif s=='side':
+            k=-1
+    else:
+        print('Type str value')
+    print(count_sum(arr,k))
